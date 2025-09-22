3b36ab5dace8adba785f2f63759f441fd6c7adae
IZERE JOSHUA <izerejoshua94@gmail.com>
2025-09-22 10:57:12 +0200

    Added filtering products feature

--- src/main/java/com/josh/store/controllers/ProductController.java
+++ src/main/java/com/josh/store/controllers/ProductController.java
@@ -2,13 +2,13 @@ package com.josh.store.controllers;
 
 import com.josh.store.Mappers.ProductMapper;
 import com.josh.store.dtos.ProductDto;
+import com.josh.store.dtos.UserDto;
 import com.josh.store.entities.Product;
 import com.josh.store.repositories.ProductRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @AllArgsConstructor
 @RestController
@@ -17,16 +17,27 @@ public class ProductController {
     private final ProductRepository productRepository;
     private final ProductMapper productMapper;
 
+    // Filtering products with optional filter parameter
     @GetMapping("")
-    public Iterable<ProductDto> allProducts() {
-        return productRepository.findAll()
-                .stream()
-                .map(productMapper::toProductDto)
-                .toList();
+    public List<ProductDto> allProducts(
+            @RequestParam(required = false) Byte categoryId
+    ) {
+        if(categoryId != null){
+            return  productRepository.findByCategoryId(categoryId) .stream()
+                    .map(productMapper::toProductDto)
+                    .toList();
+        }else {
+            return productRepository.findAll()
+                    .stream()
+                    .map(productMapper::toProductDto)
+                    .toList();
+        }
     }
 
     @GetMapping("/{id}")
-    public Product findById(@PathVariable long id) {
-        return productRepository.findById(id).orElse(null);
+    public ProductDto findById(@PathVariable long id) {
+        var product = productRepository.findById(id).orElse(null);
+        assert product != null;
+        return new ProductDto(product.getId(), product.getName(), product.getDescription(), product.getPrice(), product.getCategory().getId());
     }
 }
--- src/main/java/com/josh/store/repositories/ProductRepository.java
+++ src/main/java/com/josh/store/repositories/ProductRepository.java
@@ -3,5 +3,10 @@ package com.josh.store.repositories;
 import com.josh.store.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByCategoryId(Byte category_id);
+
+    List<Product> findByName(String name);
 }
